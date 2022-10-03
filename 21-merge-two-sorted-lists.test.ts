b279a387d02493c771d76bd3362441a700212c20
Christian Ipanaque <chris.ipanaque@gmail.com>
2022-10-03 13:57:54 -0700

    add recursive solution to #21

--- 21-merge-two-sorted-lists.test.ts
+++ 21-merge-two-sorted-lists.test.ts
@@ -1,5 +1,8 @@
 import { createLinkedListNode } from "./utils/linked-list";
-import { mergeTwoLists } from "./21-merge-two-sorted-lists";
+import {
+  mergeTwoLists,
+  mergeTwoListsRecursive,
+} from "./21-merge-two-sorted-lists";
 
 const testCases = [
   [
@@ -29,6 +32,19 @@ describe("mergeTwoLists", () => {
       const node2 = createLinkedListNode(testArray2);
       const expectedResult = createLinkedListNode(testArrayResult);
       const actualResult = mergeTwoLists(node1, node2);
+      expect(actualResult).toEqual(expectedResult);
+    }
+  );
+});
+
+describe("mergeTwoListsRecursive", () => {
+  test.each(testCases)(
+    "node1 = %p, node2 = %p, expected result = %p",
+    (testArray1, testArray2, testArrayResult) => {
+      const node1 = createLinkedListNode(testArray1);
+      const node2 = createLinkedListNode(testArray2);
+      const expectedResult = createLinkedListNode(testArrayResult);
+      const actualResult = mergeTwoListsRecursive(node1, node2);
       expect(actualResult).toEqual(expectedResult);
     }
   );
