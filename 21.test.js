08c76a8c625a086fdb3cd5bc27f8ff629e0e09c0
Christian Ipanaque <chris.ipanaque@gmail.com>
2022-10-03 10:28:13 -0700

    add test.each to test multiple test cases without boilerplate

--- 21.test.js
+++ 21.test.js
@@ -1,10 +1,28 @@
 import { createLinkedListNode } from "./data-structures/linked-list";
 import { mergeTwoLists } from "./21";
 
-test("merge [1, 2, 3] and [4, 5, 6]", () => {
-  const node1 = createLinkedListNode([1, 2, 3]);
-  const node2 = createLinkedListNode([4, 5, 6]);
-  const result = createLinkedListNode([1, 2, 3, 4, 5, 6]);
+const testCases = [
+  [
+    [1, 2, 3], // node1
+    [4, 5, 6], // node2
+    [1, 2, 3, 4, 5, 6], // expected result
+  ],
+  [
+    [6, 6, 9],
+    [0, 3, 5],
+    [0, 3, 5, 6, 6, 9],
+  ],
+];
 
-  expect(mergeTwoLists(node1, node2)).toEqual(result);
+describe("mergeTwoLists", () => {
+  test.each(testCases)(
+    "node1 = %p, node2 = %p, expected result = %p",
+    (testArray1, testArray2, testArrayResult) => {
+      const node1 = createLinkedListNode(testArray1);
+      const node2 = createLinkedListNode(testArray2);
+      const expectedResult = createLinkedListNode(testArrayResult);
+      const actualResult = mergeTwoLists(node1, node2);
+      expect(actualResult).toEqual(expectedResult);
+    }
+  );
 });
