505ff77ba74880ce99965282dc75cc5e0aa1828d
Christian Ipanaque <chris.ipanaque@gmail.com>
2022-10-06 04:54:59 -0700

    add another solution to 1 two sum to find bug

--- 0001-two-sum.ts
+++ 0001-two-sum.ts
@@ -26,3 +26,25 @@ export const twoSum = (numbers: number[], target: number) => {
 
   return [];
 };
+
+// Find Bug:
+// Which line of code can be changed so that this solution can be optimized?
+export const twoSumNonOptimized = (
+  nums: number[],
+  target: number
+): number[] => {
+  let indices: { [key: string]: number } = {};
+  let response: number[] = [];
+
+  for (let [index, num] of nums.entries()) {
+    const difference = target - num;
+
+    if (indices[difference] !== undefined) {
+      response = [index, indices[difference]!];
+    } else {
+      indices[num] = index;
+    }
+  }
+
+  return response;
+};
